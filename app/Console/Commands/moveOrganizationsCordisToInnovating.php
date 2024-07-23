9c1480388194e1c1f957a43b074a815f37e6b2d2
Ivan <ivan@innovating.works>
2024-07-23 08:52:56 +0200

    sync

--- app/Console/Commands/moveOrganizationsCordisToInnovating.php
+++ app/Console/Commands/moveOrganizationsCordisToInnovating.php
@@ -29,10 +29,11 @@ class moveOrganizationsCordisToInnovating extends Command
     public function handle()
     {
         //
-        $cordisorganizations = \App\Models\OrganizationsCordisRawData::get();
+        $cordisorganizations = \App\Models\OrganizationsCordisRawData::orderByDesc('created_at')->get();
 
         foreach($cordisorganizations as $organization){
 
+            $cif = null;
             if($organization->country == "ES" && $organization->vatNumber != ""){
                 $cif = str_replace("ES","", $organization->vatNumber);
             }elseif($organization->vatNumber != ""){
@@ -41,106 +42,112 @@ class moveOrganizationsCordisToInnovating extends Command
                 $cif = $organization->nutsCode;
             }
 
-            ###CREAMOS EMPRESA EN INNOVATING SINO EXISTE
-            $entidad = \App\Models\Entidad::where('CIF', $cif)->first();
-            if(!$entidad){
-                $entidad = \App\Models\Entidad::where('vatNumber', $cif)->first();
+            if($cif !== null){
+                ###CREAMOS EMPRESA EN INNOVATING SINO EXISTE
+                $entidad = \App\Models\Entidad::where('CIF', $cif)->first();
                 if(!$entidad){
-                    
-                    $uri = cleanUriBeforeSave(str_replace(" ","-",mb_strtolower(quitar_tildes($organization->name))));
-                    $naturalezas = array();
-                    $esuniversidad = 0;
-                    $escentro = 0;
-
-                    if($organization->activityType == "PRC"){
-                        array_push($naturalezas, "6668837");
+                    $entidad = \App\Models\Entidad::where('vatNumber', $cif)->first();
+                    if(!$entidad){
+                        
+                        $uri = cleanUriBeforeSave(str_replace(" ","-",mb_strtolower(quitar_tildes($organization->name))));
+                        $naturalezas = array();
                         $esuniversidad = 0;
                         $escentro = 0;
-                    }
-                    if($organization->activityType == "PUB"){
-                        array_push($naturalezas, "6668840");
-                        $esuniversidad = 1;
-                        $escentro = 0;
-                    }
-                    if($organization->activityType == "REC"){
-                        array_push($naturalezas, "6668838");
-                        $esuniversidad = 0;
-                        $escentro = 1;
-                    }
 
-                    $entidad = new \App\Models\Entidad();
-                    try{
-                        $entidad->Nombre = $organization->name;
-                        $entidad->Web = ($organization->organizationURL != "") ? $organization->organizationURL : "";
-                        $entidad->pais = $organization->country;
-                        $entidad->CIF = $cif;
-                        $entidad->vatNumber = ($organization->vatNumber != "") ? $organization->vatNumber : $organization->nutsCode;
-                        $entidad->uri = $uri;
-                        $entidad->Cnaes = "";
-                        $entidad->Marca = ($organization->shortName != "") ? $organization->shortName : "";
-                        $entidad->Direccion = $organization->street." ".$organization->city;
-                        $entidad->Intereses = json_encode(["I+D","Innovación","Digitalización","Cooperación","Subcontratación"], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-                        $entidad->entityUpdate = Carbon::now();
-                        $entidad->naturalezaEmpresa = (empty($naturalezas)) ? json_encode(["6668837"]) : json_encode($naturalezas);
-                        $entidad->esUniversidadPrivada = $esuniversidad;
-                        $entidad->esCentroTecnologico = $escentro;
-                        $entidad->save();
-                    }catch(Exception $e){
-                        Log::error($e->getMessage());
-                        return COMMAND::FAILURE;
-                    }
-                }
-            }
-            ###CREAMOS PARTICIPANTE DEL PROYECTO SI ES LIDER LO AÑADIMOS COMO LIDER            
-            $rawdata = \App\Models\ProjectsCordisRawData::where('project_id', $organization->project_id)->first();
-            $proyecto = \App\Models\Proyectos::where('id_raw_data', $rawdata->id)->first();
+                        if($organization->activityType == "PRC"){
+                            array_push($naturalezas, "6668837");
+                            $esuniversidad = 0;
+                            $escentro = 0;
+                        }
+                        if($organization->activityType == "PUB"){
+                            array_push($naturalezas, "6668840");
+                            $esuniversidad = 1;
+                            $escentro = 0;
+                        }
+                        if($organization->activityType == "REC"){
+                            array_push($naturalezas, "6668838");
+                            $esuniversidad = 0;
+                            $escentro = 1;
+                        }
 
-            $participante = \App\Models\Participantes::where('cif_participante', $cif)->where('id_proyecto', $proyecto->id)->first();
-
-            if(!$participante){
-                $participante = new \App\Models\Participantes();
-
-                if($rawdata && $proyecto){
-
-                    try{
-
-                        if($organization->country == "ES" && $organization->vatNumber != ""){
-                                $participante->cif_participante = str_replace("ES","", $organization->vatNumber);
-                            }elseif($organization->vatNumber != ""){
-                                $participante->cif_participante = $organization->vatNumber;
-                            }else{
-                                $participante->cif_participante = $organization->nutsCode;
-                            }
-                            $participante->nombre_participante = $organization->name;
-                            $participante->id_proyecto = $proyecto->id;
-                            $participante->ayuda_eq_socio = $organization->netEcContribution;
-                            $participante->presupuesto_socio = $organization->totalCost;
-                            $participante->save();
-
-                    }catch(Exception $e){
-                        Log::error($e->getMessage());
-                        return COMMAND::FAILURE;
+                        $entidad = new \App\Models\Entidad();
+                        try{
+                            $entidad->Nombre = $organization->name;
+                            $entidad->Web = ($organization->organizationURL != "") ? $organization->organizationURL : "";
+                            $entidad->pais = $organization->country;
+                            $entidad->CIF = $cif;
+                            $entidad->vatNumber = ($organization->vatNumber != "") ? $organization->vatNumber : $organization->nutsCode;
+                            $entidad->uri = $uri;
+                            $entidad->Cnaes = "";
+                            $entidad->Marca = ($organization->shortName != "") ? $organization->shortName : "";
+                            $entidad->Direccion = $organization->street." ".$organization->city;
+                            $entidad->Intereses = json_encode(["I+D","Innovación","Digitalización","Cooperación","Subcontratación"], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+                            $entidad->entityUpdate = Carbon::now();
+                            $entidad->naturalezaEmpresa = (empty($naturalezas)) ? json_encode(["6668837"]) : json_encode($naturalezas);
+                            $entidad->esUniversidadPrivada = $esuniversidad;
+                            $entidad->esCentroTecnologico = $escentro;
+                            $entidad->save();
+                        }catch(Exception $e){
+                            Log::error($e->getMessage());
+                            return COMMAND::FAILURE;
+                        }
                     }
                 }
 
-                if($organization->role == "coordinator"){
+                ###CREAMOS PARTICIPANTE DEL PROYECTO SI ES LIDER LO AÑADIMOS COMO LIDER            
+                $rawdata = \App\Models\ProjectsCordisRawData::where('project_id', $organization->project_id)->first();
+                $proyecto = \App\Models\Proyectos::where('id_raw_data', $rawdata->id)->first();
+
+                $participante = null;
+                
+                if($proyecto){
+                    $participante = \App\Models\Participantes::where('cif_participante', $cif)->where('id_proyecto', $proyecto->id)->first();
+                }
+
+                if(!$participante){
+                    $participante = new \App\Models\Participantes();
 
                     if($rawdata && $proyecto){
+
                         try{
 
-                            \App\Models\Proyectos::where('id_raw_data', $rawdata->id)->update([
-                                'empresaPrincipal' => $cif
-                            ]);
-                        
+                            if($organization->country == "ES" && $organization->vatNumber != ""){
+                                    $participante->cif_participante = str_replace("ES","", $organization->vatNumber);
+                                }elseif($organization->vatNumber != ""){
+                                    $participante->cif_participante = $organization->vatNumber;
+                                }else{
+                                    $participante->cif_participante = $organization->nutsCode;
+                                }
+                                $participante->nombre_participante = $organization->name;
+                                $participante->id_proyecto = $proyecto->id;
+                                $participante->ayuda_eq_socio = $organization->netEcContribution;
+                                $participante->presupuesto_socio = $organization->totalCost;
+                                $participante->save();
+
                         }catch(Exception $e){
                             Log::error($e->getMessage());
                             return COMMAND::FAILURE;
                         }
                     }
 
+                    if($organization->role == "coordinator"){
+
+                        if($rawdata && $proyecto){
+                            try{
+
+                                \App\Models\Proyectos::where('id_raw_data', $rawdata->id)->update([
+                                    'empresaPrincipal' => $cif
+                                ]);
+                            
+                            }catch(Exception $e){
+                                Log::error($e->getMessage());
+                                return COMMAND::FAILURE;
+                            }
+                        }
+
+                    }
                 }
             }
-
 
         }
 
