859e6f34aad3a9fddd29a8c905105f88c5ca2dd3
Ivan <ivan@innovating.works>
2024-09-20 10:15:25 +0200

    sync

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('import:cordis_json')->timezone('Europe/Madrid')->cron('5 4 * * 6')->withoutOverlapping();
+//Schedule::command('import:cordis_json')->timezone('Europe/Madrid')->cron('5 4 * * 6')->withoutOverlapping();
 Schedule::command('app:apply-rules-on-projects')->timezone('Europe/Madrid')->dailyAt('03:13')->withoutOverlapping();
 Schedule::command('app:move-cdti-projects-to-innovating')->timezone('Europe/Madrid')->dailyAt('05:13')->withoutOverlapping();
 Schedule::command('scrapper:cdti_proyectos')->timezone('Europe/Madrid')->dailyAt('01:13')->withoutOverlapping();
